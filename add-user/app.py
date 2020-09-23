c7fd5e00c719fc98f9e7a1b5bddecef10172e4b1
Gabriele Ciappelloni <g.ciappelloni@teamsystem.com>
2020-09-24 00:03:49 +0200

    multiple users addition

--- add-user/app.py
+++ add-user/app.py
@@ -24,38 +24,41 @@ def lambda_function(event, context):
     params = json.loads(event["body"])
 
     salt = get_SALT()
-
-    str2hash = "{}|{}|{}".format(params["login"], params["password"], salt)
-    result_hash = hashlib.md5(str2hash.encode()).hexdigest()
-    params["password"] = result_hash
-
     items = table.scan()
-    if not params.get("code"):
-        codes = sorted([int(i["code"]) for i in items.get("Items",[])])
-        if codes:
-            code = codes[-1] + 1
-        else:
-            code = 1
-        params["code"] = str(code)
-        
-    if "tenant" not in params:
-        params["tenant"] = "default"
-        
-    try:
-        with table.batch_writer() as batch:
-            batch.put_item(params)
-    except Exception:
-        import traceback
-        traceback.print_exc()
-        return {
-            "statusCode": 500,
-            "body": json.dumps({
-                "message": "Error"
-            })
-        }
+
+    for i, item in enumerate(params):
+        pwd = item.get("password", item["name"].lower())
+        str2hash = "{}|{}|{}".format(item["login"], pwd, salt)
+        result_hash = hashlib.md5(str2hash.encode()).hexdigest()
+        item["password"] = result_hash
+
+        if not item.get("code"):
+            codes = sorted([int(i["code"]) for i in items.get("Items",[])])
+            if codes:
+                code = codes[-1] + (i+1)
+            else:
+                code = 1
+            item["code"] = str(code)
+            
+        if "tenant" not in item:
+            item["tenant"] = "default"
+            
+        try:
+            with table.batch_writer() as batch:
+                batch.put_item(item)
+        except Exception:
+            log.error("error processi item: {}".format(item))
+            import traceback
+            traceback.print_exc()
+            return {
+                "statusCode": 500,
+                "body": json.dumps({
+                    "message": "Error"
+                })
+            }
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "User added"
+            "message": "Users added"
         })
     }
