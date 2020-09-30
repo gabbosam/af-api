ac29e9aed41662647bf4719065cc3471da47336f
Gabriele Ciappelloni <g.ciappelloni@teamsystem.com>
2020-09-30 22:36:20 +0200

    increase token expiration time

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "token_expiration" {
   type        = number
   description = "token expiration in minutes"
-  default     = 180
+  default     = 300
 }
 
-variable "docs_bucket_name"{
-    type=string
-    default="af-upload-docs"
+variable "docs_bucket_name" {
+  type    = string
+  default = "af-upload-docs"
 }
